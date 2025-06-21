3d9e696f29fff9e0f5a735f01772077cc5a9e393
Bernd Storck <berndstorck@hotmail.com>
2025-06-21 04:06:49 +0200

    Language files for el, ln and tr.

---
+++ getpass.lang.el
@@ -0,0 +1,10 @@
+UILANG="Ελληνικά"
+LABEL_PASSWORD_FILE="Αρχείο κωδικών πρόσβασης"
+
+ERROR="Σφάλμα"
+ERR_MISSING_CONFIGURATION_FILE="Δεν βρέθηκε αρχείο ρυθμίσεων!"
+THE_FILE="Το αρχείο"
+NOT_FOUND="δεν βρέθηκε"
+
+WARNING="Προειδοποίηση"
+DISALLOWED_OPTION="Μη επιτρεπόμενη επιλογή"
